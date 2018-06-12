185ed1405ad1ae2d212d25120c1e53c843814507
kikychn305 <kikychn@gmail.com>
2018-06-12 20:51:41 +0800

    20180612

---
+++ 剑指Offer/online_judge/t44_isContinuous.java
@@ -0,0 +1,42 @@
+package online_judge;
+/**
+ * 扑克牌顺子
+ * LL今天心情特别好,因为他去买了一副扑克牌,发现里面居然有2个大王,2个小王(一副牌原本是54张^_^)...
+ * 他随机从中抽出了5张牌,想测测自己的手气,看看能不能抽到顺子,如果抽到的话,他决定去买体育彩票,嘿嘿！！
+ * “红心A,黑桃3,小王,大王,方片5”,“Oh My God!”不是顺子.....LL不高兴了,他想了想,决定大\小 王可以看成任何数字,并且A看作1,J为11,Q为12,K为13。
+ * 上面的5张牌就可以变成“1,2,3,4,5”(大小王分别看作2和4),“So Lucky!”。LL决定去买体育彩票啦。 
+ * 现在,要求你使用这幅牌模拟上面的过程,然后告诉我们LL的运气如何。为了方便起见,你可以认为大小王是0。
+ */
+import java.util.Arrays;
+
+public class t44_isContinuous {
+	public static void main(String[] args) {
+		int[] num = { 1, 3, 0, 5, 4 };
+		System.out.print(isContinuous(num));
+	}
+
+	public static boolean isContinuous(int[] numbers) {
+		if (numbers == null || numbers.length == 0)
+			return false;
+
+		Arrays.sort(numbers);
+		int zero_num = 0;
+		for (int i = 0; i < numbers.length; ++i) {
+			if (numbers[i] == 0)
+				++zero_num;
+			else
+				break;
+		}
+
+		for (int i = zero_num; i < numbers.length - 1; ++i) {
+			if (numbers[i + 1] == numbers[i])
+				return false;
+
+			zero_num -= numbers[i + 1] - numbers[i] - 1;
+			if (zero_num < 0)
+				return false;
+		}
+
+		return true;
+	}
+}
