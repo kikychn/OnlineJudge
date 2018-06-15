c36e7668f6af80f4be011f26f44ec77fae5ac7ed
kikychn305 <kikychn@gmail.com>
2018-06-15 18:31:37 +0800

    unfinish

---
+++ 剑指Offer/online_judge/t45_LastRemaining_Solution.java
@@ -0,0 +1,12 @@
+package online_judge;
+
+public class t45_LastRemaining_Solution {
+	public static void main(String[] args) {
+		
+	}
+	
+	public static int LastRemaining_Solution(int n, int m) {
+        
+		return 0;
+    }
+}
